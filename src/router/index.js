70dbabb2e7e5d84102b79ad4ac610b26b03df85b
nuryanto <nuryantofattih@gmail.com>
2020-03-16 16:56:07 +0700

    space di index.js

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import Router from 'vue-router'
 // import { BGRender } from './BG/BG'
 // import { CBRender } from './CB/CB'
 
-// // import { CCRender } from './CC/CC'
+// import { CCRender } from './CC/CC'
 import {
   CMRender
 } from './CM/CM'
@@ -76,252 +76,252 @@ export default new Router({
     y: 0
   }),
   routes: [{
+    path: '/',
+    redirect: '/',
+    name: 'LayoutFull',
+    component: LayoutFull,
+    children: [{
       path: '/',
-      redirect: '/',
-      name: 'LayoutFull',
-      component: LayoutFull,
-      children: [{
-          path: '/',
-          name: 'Dashboard',
-          component: Dashboard,
-          meta: {
-            requiresAuth: true,
-            breadcrumbs: [{
-              name: 'Dashboard'
-            }]
-          }
-        },
-        {
-          path: '/menu',
-          name: 'Menu',
-          component: MenuPages,
-          meta: {
-            requiresAuth: true,
-            breadcrumbs: [{
-              name: 'Menu'
-            }]
-          }
-        },
-        {
-          path: '/Notification',
-          name: 'Notification',
-          component: Notification,
-          meta: {
-            requiresAuth: true,
-            breadcrumbs: [{
-              name: 'Notification'
-            }]
-          }
-        },
-        {
-          path: '/ChangePassword',
-          name: 'Change Password',
-          component: ChangePassword,
-          meta: {
-            requiresAuth: true,
-            breadcrumbs: [{
-              name: 'Change Password'
-            }]
-          }
-        },
-        {
-          path: '/UserProfile',
-          name: 'User Profile',
-          component: UserProfile,
-          meta: {
-            requiresAuth: true,
-            breadcrumbs: [{
-              name: 'User Profile'
-            }]
-          }
-        },
-        {
-          path: '/TemplatePages/ReportPage',
-          name: 'Report',
-          component: Report,
-          meta: {
-            requiresAuth: true,
-            breadcrumbs: [{
-              name: 'Report'
-            }]
-          }
-        },
-        // APRender,
-
-        // ARRender,
-        // BGRender,
-        // CBRender,
-
-        // // // CCRender,
-        CMRender,
-
-        CORender,
-
-        // // FARender,
-        FMRender,
-
-        // GLRender,
-
-        // // ICRender,
-        // // MMRender,
-        // // MURender,
-
-        // // OBRender,
-
-        // // PMRender,
-        // // PORender,
-        // SMRender,
-        SSRender,
-
-        MKRender,
-        SORender,
-
-        // TNRender,
-        // // WFRender,
-        {
-          path: '/Custom/CS_ContactAddress',
-          name: 'CS_ContactAddress',
-          component: CS_ContactAddress,
-          meta: {
-            requiresAuth: true
-          }
-        }
-        // {
-        //   path: '/AR_InvoiceDeposit',
-        //   name: 'AR_InvoiceDeposit',
-        //   component: AR_InvoiceDeposit_MasterVuex,
-        //   meta: { 
-        //     requiresAuth: true,
-        //     breadcrumbs: [
-        //       {name: 'Dashboard', link: '/'},
-        //       {name: 'Invoice Deposit', link: ''}
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/CM_CurrencyMaster',
-        //   name: 'CM_CurrencyMaster',
-        //   component: CM_CurrencyMaster_MasterVuex,
-        //   meta: {
-        //     requiresAuth: true,
-        //     breadcrumbs: [
-        //       {name: 'Dashboard', link: '/'},
-        //       {name: 'Currency Master Vuex', link: ''}
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/CM_CountryCityMaster',
-        //   name: 'CM_CountryCityMaster',
-        //   component: CM_CountryCity_Master,
-        //   meta: {
-        //     requiresAuth: true,
-        //     breadcrumbs: [
-        //       {name: 'Dashboard', link: '/'},
-        //       {name: 'Country/City Master', link: ''}
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/CM_ProvinceMaster',
-        //   name: 'CM_ProvinceMaster',
-        //   component: CM_Province_Master,
-        //   meta: {
-        //     requiresAuth: true,
-        //     breadcrumbs: [
-        //       {name: 'Dashboard', link: '/'},
-        //       {name: 'Province', link: ''}
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/CM_PortfolioMaster',
-        //   name: 'CM_PortfolioMaster',
-        //   component: CM_Portfolio_Master,
-        //   meta: {
-        //     requiresAuth: true,
-        //     breadcrumbs: [
-        //       {name: 'Dashboard', link: '/'},
-        //       {name: 'Porfolio', link: ''}
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/SS_Group',
-        //   name: 'SS_Group',
-        //   component: SS_Group_Master,
-        //   meta: {
-        //     requiresAuth: true,
-        //     breadcrumbs: [
-        //       {name: 'Dashboard', link: '/'},
-        //       {name: 'Group Entry', link: ''}
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/CM_CountryCityMasterVuex',
-        //   name: 'CM_CountryCityMasterVuex',
-        //   component: CM_CountryCity_MasterVuex,
-        //   meta: {
-        //     requiresAuth: true,
-        //     breadcrumbs: [
-        //       {name: 'Dashboard', link: '/'},
-        //       {name: 'Country/City Master', link: ''}
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/SS_PageMaster',
-        //   name: 'SS_PageMaster',
-        //   component: SS_Page_Master,
-        //   meta: {
-        //     requiresAuth: true,
-        //     breadcrumbs: [
-        //       {name: 'Dashboard', link: '/'},
-        //       {name: 'Page Master Entry', link: ''}
-        //     ]
-        //   }
-        // },
-        // {
-        //   path: '/SS_ModuleAccess',
-        //   name: 'SS_ModuleAccess',
-        //   component: SS_ModuleAccess_Master,
-        //   meta: {
-        //     requiresAuth: true,
-        //     breadcrumbs: [
-        //       {name: 'Dashboard', link: '/'},
-        //       {name: 'Module Access Entry', link: ''}
-        //     ]
-        //   }
-        // }
-      ]
+      name: 'Dashboard',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true,
+        breadcrumbs: [{
+          name: 'Dashboard'
+        }]
+      }
     },
     {
-      path: '/Map',
-      component: Map,
+      path: '/menu',
+      name: 'Menu',
+      component: MenuPages,
+      meta: {
+        requiresAuth: true,
+        breadcrumbs: [{
+          name: 'Menu'
+        }]
+      }
+    },
+    {
+      path: '/Notification',
+      name: 'Notification',
+      component: Notification,
+      meta: {
+        requiresAuth: true,
+        breadcrumbs: [{
+          name: 'Notification'
+        }]
+      }
+    },
+    {
+      path: '/ChangePassword',
+      name: 'Change Password',
+      component: ChangePassword,
+      meta: {
+        requiresAuth: true,
+        breadcrumbs: [{
+          name: 'Change Password'
+        }]
+      }
+    },
+    {
+      path: '/UserProfile',
+      name: 'User Profile',
+      component: UserProfile,
+      meta: {
+        requiresAuth: true,
+        breadcrumbs: [{
+          name: 'User Profile'
+        }]
+      }
+    },
+    {
+      path: '/TemplatePages/ReportPage',
+      name: 'Report',
+      component: Report,
+      meta: {
+        requiresAuth: true,
+        breadcrumbs: [{
+          name: 'Report'
+        }]
+      }
+    },
+      // APRender,
+
+      // ARRender,
+      // BGRender,
+      // CBRender,
+
+      // // // CCRender,
+      CMRender,
+
+      CORender,
+
+      // // FARender,
+      FMRender,
+
+      // GLRender,
+
+      // // ICRender,
+      // // MMRender,
+      // // MURender,
+
+      // // OBRender,
+
+      // // PMRender,
+      // // PORender,
+      // SMRender,
+      SSRender,
+
+      MKRender,
+      SORender,
+
+    // TNRender,
+    // // WFRender,
+    {
+      path: '/Custom/CS_ContactAddress',
+      name: 'CS_ContactAddress',
+      component: CS_ContactAddress,
       meta: {
         requiresAuth: true
       }
-    },
-    {
-      path: '/sign-in',
-      name: 'sign-in',
-      component: SignIn,
-      meta: {
-        requiresNoAuth: true
-      }
-    },
-    {
-      path: '/ReportGenerator',
-      name: 'ReportGenerator',
-      component: ReportGenerator,
-      meta: {
-        requiresAuth: true
-      }
-    },
-    {
-      path: '*',
-      component: PageNotFound
     }
+      // {
+      //   path: '/AR_InvoiceDeposit',
+      //   name: 'AR_InvoiceDeposit',
+      //   component: AR_InvoiceDeposit_MasterVuex,
+      //   meta: { 
+      //     requiresAuth: true,
+      //     breadcrumbs: [
+      //       {name: 'Dashboard', link: '/'},
+      //       {name: 'Invoice Deposit', link: ''}
+      //     ]
+      //   }
+      // },
+      // {
+      //   path: '/CM_CurrencyMaster',
+      //   name: 'CM_CurrencyMaster',
+      //   component: CM_CurrencyMaster_MasterVuex,
+      //   meta: {
+      //     requiresAuth: true,
+      //     breadcrumbs: [
+      //       {name: 'Dashboard', link: '/'},
+      //       {name: 'Currency Master Vuex', link: ''}
+      //     ]
+      //   }
+      // },
+      // {
+      //   path: '/CM_CountryCityMaster',
+      //   name: 'CM_CountryCityMaster',
+      //   component: CM_CountryCity_Master,
+      //   meta: {
+      //     requiresAuth: true,
+      //     breadcrumbs: [
+      //       {name: 'Dashboard', link: '/'},
+      //       {name: 'Country/City Master', link: ''}
+      //     ]
+      //   }
+      // },
+      // {
+      //   path: '/CM_ProvinceMaster',
+      //   name: 'CM_ProvinceMaster',
+      //   component: CM_Province_Master,
+      //   meta: {
+      //     requiresAuth: true,
+      //     breadcrumbs: [
+      //       {name: 'Dashboard', link: '/'},
+      //       {name: 'Province', link: ''}
+      //     ]
+      //   }
+      // },
+      // {
+      //   path: '/CM_PortfolioMaster',
+      //   name: 'CM_PortfolioMaster',
+      //   component: CM_Portfolio_Master,
+      //   meta: {
+      //     requiresAuth: true,
+      //     breadcrumbs: [
+      //       {name: 'Dashboard', link: '/'},
+      //       {name: 'Porfolio', link: ''}
+      //     ]
+      //   }
+      // },
+      // {
+      //   path: '/SS_Group',
+      //   name: 'SS_Group',
+      //   component: SS_Group_Master,
+      //   meta: {
+      //     requiresAuth: true,
+      //     breadcrumbs: [
+      //       {name: 'Dashboard', link: '/'},
+      //       {name: 'Group Entry', link: ''}
+      //     ]
+      //   }
+      // },
+      // {
+      //   path: '/CM_CountryCityMasterVuex',
+      //   name: 'CM_CountryCityMasterVuex',
+      //   component: CM_CountryCity_MasterVuex,
+      //   meta: {
+      //     requiresAuth: true,
+      //     breadcrumbs: [
+      //       {name: 'Dashboard', link: '/'},
+      //       {name: 'Country/City Master', link: ''}
+      //     ]
+      //   }
+      // },
+      // {
+      //   path: '/SS_PageMaster',
+      //   name: 'SS_PageMaster',
+      //   component: SS_Page_Master,
+      //   meta: {
+      //     requiresAuth: true,
+      //     breadcrumbs: [
+      //       {name: 'Dashboard', link: '/'},
+      //       {name: 'Page Master Entry', link: ''}
+      //     ]
+      //   }
+      // },
+      // {
+      //   path: '/SS_ModuleAccess',
+      //   name: 'SS_ModuleAccess',
+      //   component: SS_ModuleAccess_Master,
+      //   meta: {
+      //     requiresAuth: true,
+      //     breadcrumbs: [
+      //       {name: 'Dashboard', link: '/'},
+      //       {name: 'Module Access Entry', link: ''}
+      //     ]
+      //   }
+      // }
+    ]
+  },
+  {
+    path: '/Map',
+    component: Map,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/sign-in',
+    name: 'sign-in',
+    component: SignIn,
+    meta: {
+      requiresNoAuth: true
+    }
+  },
+  {
+    path: '/ReportGenerator',
+    name: 'ReportGenerator',
+    component: ReportGenerator,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '*',
+    component: PageNotFound
+  }
   ]
 })
