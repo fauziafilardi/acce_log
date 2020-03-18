686fe736e6cb1e019572cf657e3f2cea198454df
fauziafilardi <filardifauzia@gmail.com>
2020-03-18 18:44:13 +0700

    s

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -17,14 +17,11 @@ export default {
   mounted() {
     // this.urlDashboard = this.getDataUser().url_dashboard
     // this.urlDashboard = "settlement_dashboard";
-<<<<<<< HEAD
     // this.urlDashboard = "Dashboard2";
     // this.urlDashboard = "fleetmaintenance_dashboard";
     // this.urlDashboard = "generalafair_dashboard";
-    this.urlDashboard = "admin_dashboard";
-=======
+    // this.urlDashboard = "admin_dashboard";
     this.urlDashboard = "DashboardOP";
->>>>>>> d99039728c1fbd1dd3c93a0a9974701abf6789b7
     this.component = () => import(`./${this.urlDashboard}.vue`);
   }
 };
